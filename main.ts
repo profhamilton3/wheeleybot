6a4162d5e69d1e0639282e307680d0219c5d1eec
T. Hamilton <profhamilton3@gmail.com>
2024-10-25 11:39:05 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    sonar2 = cm_sonarbit()
+    if (sonar2 < 25) {
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    } else {
+        music.stopAllSounds()
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Nyan), music.PlaybackMode.InBackground)
+    }
+})
 function cm_sonarbit () {
     sensor_pin = DigitalPin.P8
     pins.setPull(sensor_pin, PinPullMode.PullNone)
@@ -13,17 +22,12 @@ distance = d / 58
     }
     return Math.floor(distance)
 }
-let sonar2 = 0
 let distance = 0
 let sensor_pin = 0
+let sonar2 = 0
 wuKong.setLightMode(wuKong.LightMode.BREATH)
+wuKong.lightIntensity(100)
 wuKong.stopAllMotor()
 basic.forever(function () {
-    sonar2 = cm_sonarbit()
-    if (sonar2 < 25) {
-        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-    } else {
-        music.stopAllSounds()
-        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Nyan), music.PlaybackMode.InBackground)
-    }
+	
 })
